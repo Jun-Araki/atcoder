f6b403d147a3811302694eb3cdbb7e8c44ebb4cf
Jun-Araki <jun7772006@gmail.com>
2024-02-18 17:10:29 +0900

    153a

--- 153a.rb
+++ 153a.rb
@@ -1,3 +1,16 @@
 H, A = gets.split.map(&:to_i)
 
-puts H % A == 0 ? H / A : (H / A) + 1
+# puts H % A == 0 ? H / A : (H / A) + 1
+# # A - 1は 数字が割り切れない場合をカバー
+puts (H + A - 1) / A
+
+h, a = gets.split.map(&:to_i)
+
+counter = 0
+
+while h > 0
+  h -= a
+  counter += 1
+end
+
+puts counter
