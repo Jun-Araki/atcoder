c880bc58cfbe3e66bb859b8a48cc33cf78384ebe
Jun-Araki <jun7772006@gmail.com>
2024-02-20 08:42:16 +0900

    081b

--- 081_b.rb
+++ 081_b.rb
@@ -1,15 +1,11 @@
 N = gets.to_i
 numbers = gets.split.map(&:to_i)
 
-def count_divisions(numbers)
-  count = 0
+count = 0
 
-  while numbers.all?(&:even?)
-    numbers.map! { |n| n /2 }
-    count += 1
-  end
-
-  count
+while numbers.all?(&:even?)
+  numbers.map! { |n| n /2 }
+  count += 1
 end
 
-puts count_divisions(numbers)
+puts count
