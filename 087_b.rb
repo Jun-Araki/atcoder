205384491a3fcadb4ad21c8e45af71b62ace6783
Jun-Araki <jun7772006@gmail.com>
2024-02-22 08:00:05 +0900

    a

---
+++ 087_b.rb
@@ -0,0 +1,18 @@
+A = gets.to_i
+B = gets.to_i
+C = gets.to_i
+X = gets.to_i
+
+result = 0
+
+for a in 0..A do
+  for b in 0..B do
+    for c in 0..C do
+      if 500 * a + 100 * b + 50 * c == X
+        result += 1
+      end
+    end
+  end
+end
+
+puts result
